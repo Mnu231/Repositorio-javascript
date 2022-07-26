4ec95ae1757faf3140c4c21b0e64610496065e24
RomeroJuan <romerojuanmanuel1605@gmail.com>
2022-07-26 18:58:55 -0300

    Se le agregaron funciones

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
 
 
-const respuesta = 'netscape'
+const pregunta1 = '¿Qué empresa creo java script?'
+const pregunta2 = 'Metodo que aparta el ultimo elemento de un array'
+const pregunta3 = '¿Cuál es el operador de igualdad estricta?'
+const respuesta1 = 'netscape'
+const respuesta2 = 'pop'
+const respuesta3 = '==='
+
 let respuestaUser
-
 let exit = 0
+let seleccion 
 
+    while (exit == 0) {
+        seleccion =  prompt("Eliga la dificultad (3: Dificil 2: Medio 1:facil)")
 
-while (exit == 0) {
-    
-    respuestaUser =  prompt("¿Qué empresa creo javascript").toLowerCase()
-    
-    pregunta (respuesta , respuestaUser)
-    
-    
-
-} 
-
-function pregunta( respuesta, respuestaUser ) {
-    if (respuesta == respuestaUser ) {
+            switch (seleccion) {
         
-        console.log ('correcto')
-        exit = 1
-    }
-    else{
-        console.log ('incorrecto')
+
+                case "1": 
+                    pregunta ( pregunta1 , respuesta1 , respuestaUser ) 
+                break;
         
+                case "2": 
+                    pregunta ( pregunta2 , respuesta2 , respuestaUser ) 
+                break;
+        
+                case "3": 
+                    pregunta ( pregunta3 , respuesta3 , respuestaUser ) 
+                break;
+            }
+
+
+    } 
+
+    function pregunta( preg , resp, respUse ) {
+    
+        respUse =  prompt(preg).toLowerCase()
+        
+        
+        if (resp == respUse ) {
+            
+            alert ('correcto')
+                exit = 1
+
+        }
+        else{
+
+            alert ('incorrecto, vuelva a intentarlo')
+            
+        }
     }
-}
